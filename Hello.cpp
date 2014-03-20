7df675d554f98de4af8dfaa7700b3dbbf847a000
Nikolas9896 <nhlynka@gmail.com>
2014-03-20 18:26:18 +0200

    Update Hello.cpp

--- Hello.cpp
+++ Hello.cpp
@@ -11,7 +11,7 @@ cout << " Hello World" << endl;
 cout << " Hello my friend GIT !!!" << endl;
 cout << " Are you ready for the moment!!! " << endl;
 cout << " Common Babe!!! " << endl;
-
+cout << "Man yuor are creasy!" << endl;
 return 0;
 
 }
